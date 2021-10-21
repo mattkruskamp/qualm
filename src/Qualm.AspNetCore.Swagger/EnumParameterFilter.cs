1e79efc3380294e747c47f2a2be3be0750610dd6
christanaka <christanaka@users.noreply.github.com>
2021-10-21 16:38:44 -0600

    feat: add custom enum filters for autorest

---
+++ src/Qualm.AspNetCore.Swagger/EnumParameterFilter.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.OpenApi.Any;
+using Microsoft.OpenApi.Models;
+using Qualm.AspNetCore.Swagger.Extensions;
+using Swashbuckle.AspNetCore.SwaggerGen;
+
+namespace Qualm.AspNetCore.Swagger
+{
+    public class EnumParameterFilter : IParameterFilter
+    {
+        public void Apply(OpenApiParameter parameter, ParameterFilterContext context)
+        {
+            var type = context.ApiParameterDescription.Type;
+
+            if (type.IsEnum(out var enumName))
+                parameter.Extensions.Add("x-ms-enum", new OpenApiObject()
+                {
+                    { "name", new OpenApiString(enumName ?? type.Name) },
+                    { "modelAsString", new OpenApiBoolean(false) }
+                });
+        }
+    }
+}
